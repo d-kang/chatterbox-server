934b95b85395bfe2f107bfe4e73d5f0910947803
d-kang <david.kang714@gmail.com>
2017-07-24 16:03:31 -0700

    Required Handler From Request Handler

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,32 @@
-/**
- * These rules enforce Hack Reactor's style guide.
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
- 
-module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
-};
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>chatterbox</title>
+    <link rel="stylesheet" href="styles/styles.css">
+
+    <!-- dependencies -->
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script>
+      // YOU DO NOT NEED TO EDIT THIS CODE
+      if (!/(&|\?)username=/.test(window.location.search)) {
+        var newSearch = window.location.search;
+        if (newSearch !== '' & newSearch !== '?') {
+          newSearch += '&';
+        }
+        newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
+        window.location.search = newSearch;
+      }
+    </script>
+    <!-- your scripts -->
+    <script src="env/config.js"></script>
+    <script src="scripts/app.js"></script>
+  </head>
+  <body>
+    <div id="main">
+      <h1>chatterbox</h1>
+      <!-- Your HTML goes here! -->
+    </div>
+    <div id="chats"></div>
+  </body>
+</html>
--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,7 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+exports.requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -52,7 +52,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end('Hello, World!!');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,4 +70,3 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
-
